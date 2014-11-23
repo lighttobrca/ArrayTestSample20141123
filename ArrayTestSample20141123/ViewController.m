d1af7a0a6dd1375313d2edfdbdc94021869134eb
lighttobrca <iwamototateatsu@iwamotokenatsu-no-MacBook-Air.local>
2014-11-23 09:40:04 +0900

    ArraySampleCommit
    Sample Commit

--- ArrayTestSample20141123/ViewController.m
+++ ArrayTestSample20141123/ViewController.m
@@ -18,6 +18,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *fruits;
+    fruits = @[@"apple",@"grape",@"orange",@"banana"];
+    for(int i=0;i<fruits.count;i++){
+        NSLog(@"%@",fruits[i]);
+    }
 }
 
 - (void)didReceiveMemoryWarning
